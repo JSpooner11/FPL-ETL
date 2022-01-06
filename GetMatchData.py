d444c063d04cc5eca5a60212f42d0357c6353491
JSpooner11 <96944653+JSpooner11@users.noreply.github.com>
2022-01-06 13:08:43 +0000

    Add files via upload

---
+++ GetMatchData.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import requests, json, datetime
+import boto3
+
+sqs = boto3.client('sqs')
+queue_url = 'SQS_QUEUE_URL'
+
+
+url = 'https://fantasy.premierleague.com/api/fixtures/'
+
+r = requests.get(url).json()
+
+todays_date = datetime.datetime(2022, 1, 15).strftime('%Y-%m-%d')
+#todays_date = datetime.datetime.now().strftime('%Y-%m-%d')
+
+#print(r)
+df = pd.json_normalize(r)
+
+df = df[['kickoff_time']]
+
+#convert kickoff_time to ignore the time
+#1. get the date info
+#2. convert it into a date data type
+df['kickoff_date'] = df['kickoff_time'].str[:10]
+#df['kickoff_date'] =  pd.to_datetime(df['kickoff_date'])
+df['kickoff_date'] =  pd.to_datetime(df['kickoff_date']
+                                     , format = '%Y-%m-%d')
+
+#Get time
+df['kickoff_datetime'] = df['kickoff_time'].str[11:19]
+df['kickoff_datetime'] = pd.to_datetime(df['kickoff_datetime']
+                                        , format = '%H:%M:%S')
+
+filt = df.kickoff_date == todays_date
+
+#Get max time to send notification to SQS 2 hrs after last game
+dftime = df['kickoff_datetime'][filt].max()
+dftime = dftime.time()
+
+
+response = sqs.send_message(
+    QueueUrl=queue_url,
+    DelaySeconds=10,
+    MessageAttributes={
+        'Title': {
+            'DataType': 'String',
+            'StringValue': 'FPL Gameday Finished'
+        }
+    },
+    MessageBody=(
+        'The Last game today is at ' + dftime
+    )
+)
+
+print(todays_date)
+print(df[filt])
+print(dftime)
+#print(df.head)
+
