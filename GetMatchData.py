6d8905e0cc7df8b2c6b0d16c82c48a99e0c42621
spoon <spooner.jacob11@gmail.com>
2022-01-06 15:17:50 +0000

    Removed the SQS message for now

--- GetMatchData.py
+++ GetMatchData.py
@@ -1,59 +1,44 @@
 import pandas as pd
-import requests, json, datetime
+import requests
+import json
+import datetime
 import boto3
 
-sqs = boto3.client('sqs')
-queue_url = 'SQS_QUEUE_URL'
-
+# sqs = boto3.client('sqs')
+# queue_url = 'SQS_QUEUE_URL'
 
 url = 'https://fantasy.premierleague.com/api/fixtures/'
 
 r = requests.get(url).json()
 
-todays_date = datetime.datetime(2022, 1, 15).strftime('%Y-%m-%d')
-#todays_date = datetime.datetime.now().strftime('%Y-%m-%d')
+today_date = datetime.datetime(2022, 1, 15).strftime('%Y-%m-%d')
+# today_date = datetime.datetime.now().strftime('%Y-%m-%d')
 
-#print(r)
+# print(r)
 df = pd.json_normalize(r)
 
 df = df[['kickoff_time']]
 
-#convert kickoff_time to ignore the time
-#1. get the date info
-#2. convert it into a date data type
+# convert kickoff_time to ignore the time
+# 1. get the date info
+# 2. convert it into a date data type
 df['kickoff_date'] = df['kickoff_time'].str[:10]
-#df['kickoff_date'] =  pd.to_datetime(df['kickoff_date'])
-df['kickoff_date'] =  pd.to_datetime(df['kickoff_date']
-                                     , format = '%Y-%m-%d')
+# df['kickoff_date'] =  pd.to_datetime(df['kickoff_date'])
+df['kickoff_date'] = pd.to_datetime(df['kickoff_date']
+                                    , format='%Y-%m-%d')
 
-#Get time
+# Get time
 df['kickoff_datetime'] = df['kickoff_time'].str[11:19]
 df['kickoff_datetime'] = pd.to_datetime(df['kickoff_datetime']
-                                        , format = '%H:%M:%S')
+                                        , format='%H:%M:%S')
 
-filt = df.kickoff_date == todays_date
+filt = df.kickoff_date == today_date
 
-#Get max time to send notification to SQS 2 hrs after last game
+# Get max time to send notification to SQS 2 hrs after last game
 dftime = df['kickoff_datetime'][filt].max()
 dftime = dftime.time()
 
-
-response = sqs.send_message(
-    QueueUrl=queue_url,
-    DelaySeconds=10,
-    MessageAttributes={
-        'Title': {
-            'DataType': 'String',
-            'StringValue': 'FPL Gameday Finished'
-        }
-    },
-    MessageBody=(
-        'The Last game today is at ' + dftime
-    )
-)
-
-print(todays_date)
+print(today_date)
 print(df[filt])
 print(dftime)
-#print(df.head)
-
+# print(df.head)
